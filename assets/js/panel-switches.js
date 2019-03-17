df68769554a0a936ba5d7086a9e819d0bff0ba68
Sean Young <assanges@gmail.com>
2019-03-17 10:55:53 +0800

    Issue fixed: "back to top" anchor
    解決問題：「回到頁首」錨點未有效作用

--- assets/js/panel-switches.js
+++
@@ -1,5 +0,0 @@
-function activate(panel) {
-    var allPanels = ['tab-main', 'tab-event', 'tab-contact'];
-    for (var k = 0; k < allPanels.length; k++) document.getElementById(allPanels[k]).classList.remove('is-active');
-    document.getElementById(panel).classList.add('is-active');
-}
