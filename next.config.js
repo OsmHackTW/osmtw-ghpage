7c95df442beb5a953c640e2ec39209f47093fc8e
Sean Young <assanges@gmail.com>
2026-04-11 06:00:29 +0800

    fix: update next.config.js to include output configuration for static exports

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 export default {
   // Use actual domain for production and localhost for development.
   assetPrefix: isProd ? 'https://osm.tw' : '',
+  // `next export` has been removed in favor of 'output: export' in next.config.js ,
+  // see: https://nextjs.org/docs/app/guides/static-exports
+  output: 'export',
 }
