4dd29a3d22218b55713d6f73da31c08a8f701d2f
Sean Young <assanges@gmail.com>
2026-04-11 05:56:17 +0800

    fix: change module.exports to export default in next.config.js (cjs → esm)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = {
+export default {
   // Use actual domain for production and localhost for development.
   assetPrefix: isProd ? 'https://osm.tw' : '',
 }
